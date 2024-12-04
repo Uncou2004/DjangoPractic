736d217732bd1ccc51c0d0e787b68c7d2fae78ad
gr433_pagmi <georgij.panfilov.76@gmail.com>
2024-12-04 13:53:09 +0700

    User authentication and authorization (up to models)

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 import uuid # Required for unique book instances
 
+
 class MyModelName(models.Model):
     """Типичный класс модели, производный от класса Model."""
 
