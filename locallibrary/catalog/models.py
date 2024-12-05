1963cb86038a888b51f4cea19af4e35ab4f156af
gr433_pagmi <georgij.panfilov.76@gmail.com>
2024-12-05 11:35:29 +0700

    Russification

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -5,32 +5,12 @@ from django.contrib.auth.models import User
 from datetime import date
 
 
-class MyModelName(models.Model):
-    """Типичный класс модели, производный от класса Model."""
-
-    # Поля
-    my_field_name = models.CharField(max_length=20, help_text='Введите описание поля')
-    # …
-
-    # Метаданные
-    class Meta:
-        ordering = ['-my_field_name']
-
-    # Methods
-    def get_absolute_url(self):
-        """Возвращает URL-адрес для доступа к определенному экземпляру MyModelName."""
-        return reverse('model-detail-view', args=[str(self.id)])
-
-    def __str__(self):
-        """Строка для представления объекта MyModelName (например, в административной панели и т.д.)."""
-        return self.my_field_name
-
 class Genre(models.Model): #Модель жанра
     """
     Model representing a book genre (e.g. Science Fiction, Non Fiction).
     """
     name = models.CharField(max_length=200,
-                            help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+                            help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)", verbose_name=("Имя"))
 
     def __str__(self):
         """
@@ -42,14 +22,14 @@ class Book(models.Model):  #Модель кинги
     """
     Model representing a book (but not a specific copy of a book).
     """
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200, verbose_name=("Название"))
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, verbose_name=("Автор"))
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file.
-    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book", verbose_name=("Описание"))
     isbn = models.CharField('ISBN', max_length=13,
                             help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book", verbose_name=("Жанр"))
 
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
@@ -81,20 +61,20 @@ class BookInstance(models.Model):  #Модель BookInstance
     """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,
                           help_text="Unique ID for this particular book across whole library")
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-    imprint = models.CharField(max_length=200)
-    due_back = models.DateField(null=True, blank=True)
-    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True, verbose_name=("Книга"))
+    imprint = models.CharField(max_length=200, verbose_name=("Год выпуска"))
+    due_back = models.DateField(null=True, blank=True, verbose_name=("Дата возврата"))
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, verbose_name=("Заёмщик"))
 
     LOAN_STATUS = (
-        ('m', 'Maintenance'),
-        ('o', 'On loan'),
-        ('a', 'Available'),
-        ('r', 'Reserved'),
+        ('m', 'На тех. обслуживании'),
+        ('o', 'Доступна'),
+        ('a', 'Взята'),
+        ('r', 'Забронирована'),
     )
 
     status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m',
-                              help_text='Book availability')
+                              help_text='Book availability', verbose_name=("Состояние"))
 
     class Meta:
         ordering = ["due_back"]
@@ -115,10 +95,10 @@ class Author(models.Model):  #Модель автора
     """
     Model representing an author.
     """
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    first_name = models.CharField(max_length=100, verbose_name=("Имя автора"))
+    last_name = models.CharField(max_length=100, verbose_name=("Фамилия автора"))
+    date_of_birth = models.DateField(null=True, blank=True, verbose_name=("Дата рождения"))
+    date_of_death = models.DateField('Дата смерти', null=True, blank=True)
 
     def get_absolute_url(self):
         """
